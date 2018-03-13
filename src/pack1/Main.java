07566bfaaf1ee268d6534649e5611fd5983825fc
David Endrych <david.endrych@gmail.com>
2018-03-13 11:14:09 +0100

    Remove test files

--- src/pack1/Main.java
+++
@@ -1,9 +0,0 @@
-package pack1;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Test");
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.Add(5,1));
-    }
-}
