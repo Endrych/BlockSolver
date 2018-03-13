a3fbc1d188ccbda26938fa6079c6faa8ed8fbc3f
David Endrych <david.endrych@gmail.com>
2018-03-13 11:05:06 +0100

    Settings

---
+++ src/pack1/Calculator.java
@@ -0,0 +1,23 @@
+package pack1;
+
+public class Calculator {
+    /**
+     * Operace scitani
+     * @param a prvni cislo
+     * @param b druhe cislo
+     * @return vraci soucet
+     */
+    public int Add(int a, int b){
+        return a+b;
+    }
+
+    /**
+     * Operace odcitani
+     * @param a prvni cislo
+     * @param b druhe cislo
+     * @return vraci rozdil cisel
+     */
+    public int Sub(int a, int b){
+        return a-b;
+    }
+}
---
+++ src/pack1/Main.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Test");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.Add(5,1));
+    }
+}
