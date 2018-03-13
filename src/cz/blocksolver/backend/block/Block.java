c4f5304d6bdb94c0b7e366965148167de7620e68
Your Name <kukin1996@seznam.cz>
2018-03-13 17:18:44 +0100

    Add abstract class Block

---
+++ src/cz/blocksolver/backend/block/Block.java
@@ -0,0 +1,84 @@
+package cz.blocksolver.backend.block;
+
+
+import cz.blocksolver.backend.port.InputPort;
+import cz.blocksolver.backend.port.OutputPort;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public abstract class Block implements IBlock{
+    protected String name;
+    protected Integer x, y, width, height;
+    protected Collection<InputPort> inputPorts;
+    protected OutputPort outputPort;
+
+    public Block(String name, Integer x, Integer y, Integer width, Integer height){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.inputPorts = new ArrayList<>();
+    }
+
+    @Override
+    public void executeBlock() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Collection<InputPort> getInputPorts() {
+        return inputPorts;
+    }
+
+    public void setInputPorts(InputPort inputPort) {
+        inputPorts.add(inputPort);
+    }
+
+    public OutputPort getOutputPort() {
+        return outputPort;
+    }
+
+    public void setOutputPort(OutputPort outputPort) {
+        this.outputPort = outputPort;
+    }
+}
