0ea16ef66f1dbbd7af96d5f69c408124f280565e
David Endrych <david.endrych@gmail.com>
2018-03-13 11:50:32 +0100

    Implement Port class

--- src/cz/blocksolver/backend/port/Port.java
+++ src/cz/blocksolver/backend/port/Port.java
@@ -2,5 +2,30 @@ package cz.blocksolver.backend.port;
 
 public abstract class Port {
     private String name;
+    private PortType type;
+    private Double value;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PortType getType() {
+        return type;
+    }
+
+    public void setType(PortType type) {
+        this.type = type;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
 }
