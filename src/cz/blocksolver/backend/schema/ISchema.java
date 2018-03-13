bdae54d46324f93131bab7ed7dce2a5300bfbac0
David Endrych <david.endrych@gmail.com>
2018-03-13 22:11:49 +0100

    Remove runSchema, debugSchema

--- src/cz/blocksolver/backend/schema/ISchema.java
+++ src/cz/blocksolver/backend/schema/ISchema.java
@@ -3,8 +3,6 @@ package cz.blocksolver.backend.schema;
 import cz.blocksolver.backend.block.Block;
 
 public interface ISchema {
-    void runSchema();
-    void debugSchema();
     void addBlock(Block block);
     void removeBlock(Block block);
     void checkForCycles();
