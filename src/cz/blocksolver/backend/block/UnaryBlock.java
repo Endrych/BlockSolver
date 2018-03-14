4d7bd4c5d7a9986b4c65c8f6ecf16677ee68400a
Your Name <kukin1996@seznam.cz>
2018-03-14 19:20:44 +0100

    Implemet UnaryBlock

---
+++ src/cz/blocksolver/backend/block/UnaryBlock.java
@@ -0,0 +1,45 @@
+package cz.blocksolver.backend.block;
+
+import cz.blocksolver.backend.port.InputPort;
+import cz.blocksolver.backend.port.OutputPort;
+import cz.blocksolver.backend.port.PortType;
+
+import java.util.Collection;
+
+public class UnaryBlock extends Block{
+
+    private IUnaryOperation operation;
+    private Collection<IUnaryOperation> listOfOperations;
+    private InputPort inputPort;
+    private OperationResult result;
+
+    public UnaryBlock(String name, Integer x, Integer y, Integer width, Integer height, IUnaryOperation operation) {
+        super(name, x, y, width, height);
+        this.operation = operation;
+        inputPort = new InputPort(PortType.NUMBER, 0.0, 1 );
+        initializeListOfOperations();
+    }
+
+    private void initializeListOfOperations() {
+
+    }
+
+    @Override
+    public void executeBlock() {
+        result = this.operation.executeOperation(inputPort.getValue());
+        outputPort.setValue(result.getResult());
+    }
+
+    public InputPort getInputPort(Integer index){
+        if(index == 1){
+            return inputPort;
+        }else{
+            throw new IndexOutOfBoundsException("getInputPort in GoniometricBlock\n");
+        }
+    }
+
+    public void changeOperation(IUnaryOperation operation){
+        this.operation = operation;
+    }
+
+}
