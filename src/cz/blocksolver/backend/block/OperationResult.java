2ec8844026111fab3a796fce34ad7877752ab3a6
Your Name <kukin1996@seznam.cz>
2018-03-14 15:55:38 +0100

    Modify OperationResult added PortType variable

--- src/cz/blocksolver/backend/block/OperationResult.java
+++ src/cz/blocksolver/backend/block/OperationResult.java
@@ -1,15 +1,25 @@
 package cz.blocksolver.backend.block;
 
+import cz.blocksolver.backend.port.PortType;
+
 public class OperationResult {
     private Double result;
     private Boolean status;
-
+    private PortType type;
     public OperationResult(Double result, Boolean status){
         this.result = result;
         this.status = status;
+        this.type = PortType.NUMBER;
     }
     public OperationResult(Double result){
         this.result = result;
+        this.type = PortType.NUMBER;
+        setStatus();
+    }
+
+    public OperationResult(Double result, PortType type){
+        this.result = result;
+        this.type = type;
         setStatus();
     }
 
@@ -20,6 +30,10 @@ public class OperationResult {
         else{
             this.status = true;
         }
+    }
+
+    public PortType getPortType(){
+        return type;
     }
 
     public Double getResult() {
